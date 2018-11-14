a547a03a4aa88fd2ab280c37b69f71c9c162df1d
Phil Maurer <philmau@gmail.com>
2018-11-14 16:56:55 +0000

    Fix parsing standard post requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const bodyParser = require('body-parser');
 const path = require('path');
 const requests = require('./lib/requests');
 const fs = require('fs');
@@ -8,25 +9,30 @@ const app = express();
 const upload = multer({dest: 'uploads', fileFilter: (req, file, cb) => cb(null, false)});
 
 app.use(upload.any());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('/', (req, res) => {
 	let request = new requests(req);
 	req.is('json') ? res.send({request}) : res.render('index', {request});
-})
+});
 
 app.post('/save', (req, res) => {
 	let hash = req.body.hash;
 	let data = req.body.serialised;
-	requests.saveRequest('./cached', hash, data)
-	res.redirect('/' + hash);
-})
+	requests.saveRequest('./cached', hash, data);
+	res.redirect('/saved/' + hash);
+});
 
-app.get('/:hash', (req, res, next) => {
+app.get('/saved/:hash', (req, res, next) => {
 	let file = fs.readFileSync('./cached/' + req.params.hash);
 	let request = JSON.parse(file);
 	req.is('json') ? res.send({request}) : res.render('index', {request});
-})
+});
 
 module.exports = app;
