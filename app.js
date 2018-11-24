546450881bbb84064b0a7b168300454b91840f61
Phil Maurer <philmau@gmail.com>
2018-11-24 21:20:40 +0000

    re-write request parser, fix saving

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const requests = require('./lib/requests');
+const Requestor = require('./lib/requestor');
 const fs = require('fs');
 
 const app = express();
@@ -14,26 +14,58 @@ app.use(express.urlencoded());
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.all('/', (req, res) => {
-	let request = new requests(req);
-	res.render('index', {request});
+app.all('/', (req, res, next) => {
+	try {
+		let requestor = new Requestor().fromRequest(req);
+		res.render('index', {requestor});
+	} catch(err) {
+		next(err);
+	}
 });
 
-app.post('/save', (req, res) => {
-	let hash = req.body.hash;
-	let data = req.body.serialised;
-	requests.saveRequest('./cached', hash, data);
-	res.redirect('/saved/' + hash);
+app.post('/save', (req, res, next) => {
+	try {	
+		let hash = req.body.hash;
+		let serialised = req.body.serialised;
+		let resultingHash = Requestor.saveRequest('./cached', hash, serialised);
+		if (resultingHash) {
+			return res.redirect('/saved/' + resultingHash)
+		}
+		res.redirect('/');
+	} catch(err) {
+		next(err);
+	}
 });
 
 app.get('/saved/:hash', (req, res, next) => {
-	let file = fs.readFileSync('./cached/' + req.params.hash);
-	let request = JSON.parse(file);
-	res.render('index', {request});
+	try {		
+		if (fs.existsSync('./cached/' + req.params.hash)) {
+			let file = fs.readFileSync('./cached/' + req.params.hash);
+			let serialised = JSON.parse(file);
+			let requestor = new Requestor().fromSerialised(req, serialised);
+			res.render('index', {requestor});
+		} else {
+			res.render('error', {title:'Request expired', message: 'The saved request you are after has expired.'});
+		}
+	} catch(err) {
+		next(err);
+	}
 });
 
 app.get('/test', (req, res) => {
   res.render('test');
 });
+
+// 404 error handler
+app.use((req, res) => {
+	res.render('error', {title: '404', message: 'Page not found'})
+});
+
+// Proper error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+	res.status(500)
+	res.render('error', {title: '500', message: err})
+})
 
 module.exports = app;
