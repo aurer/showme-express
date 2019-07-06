cc8b5734951938da3ed04094c46d3860b3fd2565
Phil Maurer <philmau@gmail.com>
2019-07-06 23:24:18 +0100

    Upgrade gulpfile for v4

--- gulpfile.js
+++ gulpfile.js
@@ -4,61 +4,66 @@ const less = require('gulp-less');
 const uglify = require('gulp-uglify');
 const insert = require('gulp-insert');
 const plumber = require('gulp-plumber');
-const fs = require('fs');
 const config = require('./config.json');
 
 const src = './assets';
 const dist = './public';
 
 // Compile less
-gulp.task('less', function() {
+function css(cb) {
 	gulp.src([`${src}/less/app.less`])
 		.pipe(plumber())
 		.pipe(less())
 		.pipe(gulp.dest(`${dist}/css`))
 		.pipe(server.stream());
-});
+		cb();
+};
 
 // Compile javascrip
-gulp.task('js', function() {
+function js(cb) {
 	gulp.src([`${src}/js/main.js`])
 		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(gulp.dest(`${dist}/js`))
 		.pipe(server.stream());
-});
+		cb();
+};
 
 // Copy graphics
-gulp.task('gfx', function() {
+function gfx(cb) {
 	gulp.src(`${src}/gfx/*`)
-		.pipe(gulp.dest(`${dist}/gfx`))
-})
+		.pipe(gulp.dest(`${dist}/gfx`));
+		cb();
+};
 
 // Compile bookmarklets
-gulp.task('bookmarklets', function() {
+function bookmarklets(cb) {
 	gulp.src([`${src}/bookmarklets/*`])
 		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(insert.prepend('javascript:(function(){'))
 		.pipe(insert.append('})();'))
 		.pipe(gulp.dest(`${dist}/bookmarklets`));
-});
+		cb();
+};
 
 // Watch for changes
-gulp.task('watch', function() {
+function watch(cb) {
 	gulp.watch(`${src}/js/**/*.js`, ['js']);
 	gulp.watch(`${src}/less/**/*.less`, ['less']);
 	gulp.watch(`${src}/gfx/*`, ['gfx']);
 	gulp.watch(`${src}/bookmarklets/*.js`, ['bookmarklets']);
-});
+	cb();
+};
 
 // Setup local server with injection
-gulp.task('serve', function() {
+function serve(cb) {
 	server.init({
 		proxy: 'localhost:' + config.port,
 		notify: false
 	});
-});
+	cb();
+};
 
-gulp.task('default', ['less', 'js', 'gfx', 'bookmarklets']);
-gulp.task('dev', ['default', 'watch', 'serve']);
+exports.default = gulp.parallel(css, js, gfx, bookmarklets);
+exports.dev = gulp.parallel(css, js, gfx, bookmarklets, watch, serve);
