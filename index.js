bb91ee99df7362346723ad98d9c17e1cb4c50562
Phil Maurer <philmau@gmail.com>
2019-07-06 23:25:12 +0100

    Improve startup log message

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./app')
 const config = require('./config.json')
 
 app.listen(config.port, () => {
-	console.log(`App running on port ${config.port}`);
+	console.log(`App running on port http://localhost:${config.port}`);
 });
