9fd6bc01246eb96de0decd4193ab3907de19363c
Phil Maurer <philmau@gmail.com>
2020-06-10 21:15:40 +0100

    Clean up gulpfile and packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,64 @@
 const server = require('browser-sync');
-const gulp = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const less = require('gulp-less');
 const uglify = require('gulp-uglify');
 const insert = require('gulp-insert');
 const plumber = require('gulp-plumber');
 const config = require('./config.json');
 
-const src = './assets';
-const dist = './public';
+const srcDir = './assets';
+const distDir = './public';
 
 // Compile less
 function css(cb) {
-	gulp.src([`${src}/less/app.less`])
+	return src([`${srcDir}/less/app.less`])
 		.pipe(plumber())
 		.pipe(less())
-		.pipe(gulp.dest(`${dist}/css`))
+		.pipe(dest(`${distDir}/css`))
 		.pipe(server.stream());
-		cb();
-};
+}
 
 // Compile javascrip
 function js(cb) {
-	gulp.src([`${src}/js/main.js`])
+	return src([`${srcDir}/js/main.js`])
 		.pipe(plumber())
 		.pipe(uglify())
-		.pipe(gulp.dest(`${dist}/js`))
+		.pipe(dest(`${distDir}/js`))
 		.pipe(server.stream());
-		cb();
-};
+}
 
 // Copy graphics
 function gfx(cb) {
-	gulp.src(`${src}/gfx/*`)
-		.pipe(gulp.dest(`${dist}/gfx`));
-		cb();
-};
+	return src(`${srcDir}/gfx/*`).pipe(dest(`${distDir}/gfx`));
+}
 
 // Compile bookmarklets
 function bookmarklets(cb) {
-	gulp.src([`${src}/bookmarklets/*`])
+	return src([`${srcDir}/bookmarklets/*`])
 		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(insert.prepend('javascript:(function(){'))
 		.pipe(insert.append('})();'))
-		.pipe(gulp.dest(`${dist}/bookmarklets`));
-		cb();
-};
+		.pipe(dest(`${distDir}/bookmarklets`));
+}
 
 // Watch for changes
-function watch(cb) {
-	gulp.watch(`${src}/js/**/*.js`, ['js']);
-	gulp.watch(`${src}/less/**/*.less`, ['less']);
-	gulp.watch(`${src}/gfx/*`, ['gfx']);
-	gulp.watch(`${src}/bookmarklets/*.js`, ['bookmarklets']);
+function watchFiles(cb) {
+	watch(`${srcDir}/js/**/*.js`, js);
+	watch(`${srcDir}/less/*.less`, css);
+	watch(`${srcDir}/gfx/*`, gfx);
+	watch(`${srcDir}/bookmarklets/*.js`, bookmarklets);
 	cb();
-};
+}
 
 // Setup local server with injection
 function serve(cb) {
 	server.init({
 		proxy: 'localhost:' + config.port,
-		notify: false
+		notify: false,
 	});
 	cb();
-};
+}
 
-exports.default = gulp.parallel(css, js, gfx, bookmarklets);
-exports.dev = gulp.parallel(css, js, gfx, bookmarklets, watch, serve);
+exports.default = parallel(css, js, gfx, bookmarklets);
+exports.dev = parallel(css, js, gfx, bookmarklets, watchFiles, serve);
